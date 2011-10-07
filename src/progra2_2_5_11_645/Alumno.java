d7879eb7b6ecd4ab3bf3fe5123dbf15be70e2e17
Carlos Gochez <cgochez@lawgical.com>
2011-10-07 07:52:21 -0600

    aqui agregue hola mundo y la clase alumno

---
+++ src/progra2_2_5_11_645/Alumno.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra2_2_5_11_645;
+
+/**
+ *
+ * @author CarlosArmando
+ */
+public class Alumno {
+    
+}
--- src/progra2_2_5_11_645/Progra2_2_5_11_645.java
+++ src/progra2_2_5_11_645/Progra2_2_5_11_645.java
@@ -15,5 +15,6 @@ public class Progra2_2_5_11_645 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola Mundo");
     }
 }
