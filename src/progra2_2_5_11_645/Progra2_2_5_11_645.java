46bfa725bfbcd99f27d43361fc61d4746071eb75
Carlos Gochez <cgochez@lawgical.com>
2011-10-07 07:29:38 -0600

    este es mi primer commit

---
+++ src/progra2_2_5_11_645/Progra2_2_5_11_645.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra2_2_5_11_645;
+
+/**
+ *
+ * @author CarlosArmando
+ */
+public class Progra2_2_5_11_645 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
