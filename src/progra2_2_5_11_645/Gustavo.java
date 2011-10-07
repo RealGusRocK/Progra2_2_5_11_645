6f62c008160a827d94f621badb09203e89e80e6e
Gustavo <gustavo_ocampo@unitec.edu>
2011-10-07 08:57:15 -0600

    agregue clase Gustavo

---
+++ src/progra2_2_5_11_645/Gustavo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra2_2_5_11_645;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Gustavo {
+    public static void main(String [] args){
+        System.out.println("Gustavo Ocampo");
+    }
+}
